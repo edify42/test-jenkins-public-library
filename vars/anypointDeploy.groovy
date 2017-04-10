0c97b86208a556debfc1713ae244d54f080c2e23
Edward Kim <edwkim@deloitte.com.au>
2017-04-10 17:17:49 +1000

    maven deploy Signed-off-by: Edward Kim <edwkim@deloitte.com.au>

---
+++ vars/anypointDeploy.groovy
@@ -0,0 +1,15 @@
+def call(args = null) {
+    args = args ?: [:]
+    // Populate from input map
+    def repoPath = args.get("repoPath", null)
+    def USERNAME = args.get("username", null)
+    String branchName = "${env.BRANCH_NAME}"
+    echo "${branchName} is the branch name"
+    def artifactory = "http://172.17.0.1:8020/artifactory"
+
+    if (!repoPath) {
+        throw new Exception("Please specify a repoPath and name: `mavenDeploy(repoPath: 'libs-snapshot-local')`")
+    }
+
+    sh "${tool 'M3'}/bin/mvn -DaltDeploymentRepository='JCU Artifactory::default::${artifactory}/${repoPath}' -Dmaven.test.skip.exec=true -Denv.user=${USERNAME} -Denv.pass=${PASSWORD} -Denv.deployenv=Test deploy"
+}
